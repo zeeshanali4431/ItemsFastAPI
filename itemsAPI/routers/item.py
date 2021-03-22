f8ef5a4b2b4fc246217d5c8d463af74c5164a7ff
shani <shani4431@yahoo.com>
2021-03-22 22:12:13 +0500

    13-Completed the authentication process

--- itemsAPI/routers/item.py
+++ itemsAPI/routers/item.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Response, Depends
-from .. import schemas, database, models
+from .. import schemas, database, models, oauth2
 from ..crud import itemCrud
 from sqlalchemy.orm import Session
 from typing import List
@@ -19,33 +19,33 @@ router= APIRouter(
 #Route for the insert item in database
 
 @router.post('/', response_model=schemas.Items)
-def createItem(request: schemas.Items, db: Session=Depends(get_db)):
+def createItem(request: schemas.Items, db: Session=Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return itemCrud.create_item(request, db)
 
 
 #Route for the insert item in database
 
 @router.get('/', response_model=List[schemas.ShowItems])
-def searchItem(item: str, db: Session=Depends(get_db)):
+def searchItem(item: str, db: Session=Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return itemCrud.search_item(item, db)
 
 
 #Route for the list of all items in database
 
 @router.get('/all', response_model=List[schemas.ShowItems])
-def getAllItem(db: Session=Depends(get_db)):
+def getAllItem(db: Session=Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return itemCrud.get_all_items(db)
 
 
 #Route for the delete item in database
 
 @router.delete('/{id}')
-def deleteItem(id: int, db: Session=Depends(get_db)):
+def deleteItem(id: int, db: Session=Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return itemCrud.delete_item(id, db)
 
 
 #Route for the update item in database
 
 @router.put('/update')
-def updateItem(id: int, request: schemas.Items, db: Session=Depends(get_db)):
+def updateItem(id: int, request: schemas.Items, db: Session=Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return itemCrud.update_item(id, request, db)
--- itemsAPI/token.py
+++ itemsAPI/token.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from . import schemas
 
 
 
@@ -17,3 +18,14 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_token(token: str, credentials_exception):
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            email: str = payload.get("sub")
+            if email is None:
+                raise credentials_exception
+            token_data = schemas.TokenData(email=email)
+        except JWTError:
+            raise credentials_exception
