e21c264946bdc0bda338fcdeb7ce716b76709834
shani <shani4431@yahoo.com>
2021-03-22 15:28:08 +0500

    11-Create a module hashing that converts the user's plain password into hashed_password

--- itemsAPI/crud/userCrud.py
+++ itemsAPI/crud/userCrud.py
@@ -1,12 +1,13 @@
 from fastapi import Depends
 from .. import schemas, models
 from sqlalchemy.orm import Session
+from ..hashing import Hash
 
 
 #Method to create the user in database
 
 def createUser(request: schemas.User, db: Session):
-    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
